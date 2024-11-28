8ecde1f94eeac200b789e5516ac67dfb72c51ca8
ewrosky <98802240+ewrosky@users.noreply.github.com>
2024-11-28 02:33:47 -0300

    playwright tests

--- tests/pokedex.spec.js
+++ tests/pokedex.spec.js
@@ -2,12 +2,12 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080')
+    await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
   test('pokemon can be navigated to', async ({ page }) => {
-    await page.goto('http://localhost:8080')
+    await page.goto('/')
     await page.getByText('ivysaur').click()
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
